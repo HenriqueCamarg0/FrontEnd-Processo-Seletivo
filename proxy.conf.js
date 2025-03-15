efae12402550238af82c56b46f585365cd8b4f40
Henrique <henry2camargo@gmail.com>
2025-03-15 09:55:58 -0300

    Correção

---
+++ proxy.conf.js
@@ -0,0 +1,12 @@
+const { log } = require("@angular-devkit/build-angular/src/builders/ssr-dev-server");
+
+const PROXY_CONFIG = [
+    {
+        context: ["/api"],
+        target: "http://localhost:8080",
+        secure: false,
+        logLevel: "debug"
+    }
+];
+
+module.exports = PROXY_CONFIG;
